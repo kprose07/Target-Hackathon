13d2f4762cad2ae2e1a67e36d619a1bd7e746702
Khaylah <100654935+kprose07@users.noreply.github.com>
2022-02-28 20:27:23 -0600

    Add files via upload

---
+++ Target/Home.js
@@ -0,0 +1,192 @@
+// JavaScript source code
+//alert("Test");
+html, body {
+    font - family: Tajawal;
+    max - width: 100 %;
+    overflow - x: hidden;
+}
+
+.navbar {
+    background - color: #466365;
+    height: 78px;
+    padding: 3px 2px;
+    position: relative;
+    -webkit - user - select: none;
+
+}
+.navbar - nav a{
+    text - decoration: none;
+    color: white;
+    width: 100px;
+    text - align: center;
+}
+    .navbar - nav a: hover {
+    background - color: #D4AC0D;
+    color: #FDEDEC;
+}
+
+    .navbar - nav a.active {
+    background - color: white;
+    color: #D4AC0D;
+}
+.navbar - toggler{
+    background: #ffffff;
+    border: none;
+    padding: 10px 6px;
+    outline: none;
+}
+    .navbar - toggler span {
+    display: block;
+    width: 22px;
+    height: 2px;
+    border: 1px;
+    background: #D4AC0D;
+}
+    .navbar - toggler span + span {
+    margin - top: 4px;
+    width: 22px;
+}
+    .navbar - toggler span + span + span{
+    width: 22px;
+}
+.navbar - collapse a {
+    background: #466365;
+    color: white;
+    text - align: center;
+    width: 100px;
+    float: right;
+}
+
+
+.im img {
+    position: absolute;
+    width: 100 %;
+    margin - top: 2 %;
+    height: auto;
+    -webkit - user - select: none;
+}
+
+
+.im p {
+    color: #466365;
+    font - size: 5vw;
+    position: absolute;
+    height: auto;
+    margin - top: -11 %;
+    left: 10 %;
+}
+.ball img {
+    position: absolute;
+    width: 20 %;
+    margin - top: -11.8 %;
+    left: 76 %;
+    height: auto;
+}
+
+.im1 img{
+    position: absolute;
+    margin - top: -17.4 %;
+    width: 10 %;
+    height: auto;
+}
+
+.im2 img {
+    position: absolute;
+    width: 95 %;
+    margin - top: 5 %;
+    height: auto;
+}
+.students img {
+    position: absolute;
+    width: 17 %;
+    margin - top: 5.59 %;
+    height: auto;
+    left: 2.3 %;
+}
+.im2 p {
+    font - size: 3.5vw;
+    width: 60 %;
+    color: #466365;
+    position: absolute;
+    height: auto;
+    margin - top: 7.5 %;
+    left: 25 %;
+}
+.pb2 img {
+    position: absolute;
+    width: 11 %;
+    height: auto;
+    margin - top: 15 %;
+    height: auto;
+    left: 92 %;
+}
+.bottom1 img{
+    position: absolute;
+    width: 100 %;
+    height: 40 %;
+    margin - top: 25 %;
+
+}
+/*
+.uphalf{
+    position: absolute;
+    width: 95%;
+    padding:10px;
+   
+
+}
+.uphalf img {
+        width:100%; 
+        top:60px;
+        position:relative;
+    }
+
+.uphalf p {
+    color: #466365;
+    font-size:50px;
+    position:relative;
+    bottom:25px;
+    left:60px;
+}
+.half {
+    position: absolute;
+    width: 95%;
+}
+.half img {
+    position:relative;
+    width:804px; 
+    top:300px;
+   
+ }
+    .half p {
+        color: #466365;
+        font-size: 25px;
+        position: relative;
+        top: 180px;
+        left: 37%;
+    }
+
+    */
+body:: -webkit - scrollbar {
+    width: .8em;
+    border - radius: 100px;
+}
+
+body:: -webkit - scrollbar - track {
+    box - shadow: inset 0 0 6px #555;
+}
+
+body:: -webkit - scrollbar - thumb {
+
+    border - radius: 100vw;
+    box - shadow: inset 0 0 6px #E6E5FF;
+
+    outline: 1px solid slategrey;
+    margin - top: 100px;
+    margin - bottom: 100px;
+}
+body:: -webkit - scrollbar - thumb: hover {
+    background - color: #466365;
+    outline: 1px solid slategrey;
+}
+
